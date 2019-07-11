6a8288fc9473920d2d386e88269d4884ef6959af
Noor <noortazc@gmail.com>
2019-07-11 06:39:12 -0400

    shows mobile framework modified

--- sprint-2/js/script.js
+++
@@ -1,6 +0,0 @@
-var message = document.querySelector('.comment-box__message--input').textContent;
-
-message.style.color = red;
-
-var newComment = document.createElement('p');
-newComment.textContent = message;
