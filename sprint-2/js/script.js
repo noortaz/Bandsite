ed4dff5968f886ac5efae3b832bda9fba1c83d1e
Noor <noortazc@gmail.com>
2019-07-13 15:42:56 -0400

    fixed some bugs of display function

--- sprint-2/js/script.js
+++ sprint-2/js/script.js
@@ -1,65 +1,80 @@
 
-let newComment = [];
 
-let newSection = document.querySelector('.comments');
+
+
 
 function createCommentDiv (object) {
+
+    //create new comment section
+    let newSection = document.querySelector('.comments');
     let newDiv = document.createElement('div');
+
+    // add new class
     newDiv.classList = 'comments__box';
+
+    // save the html portion inside a variable as a string
     let html = '<img class="comments__box__image" src="" alt=""><div class="comments__box__content"><div class="comments__box__name">%Name%</div><div class="comments__box__date">%12/11/2019%</div><div class="comments__box__message">%comment-message% </div></div >'
+
+    // replace the name, message and date of comment with the actual value
     let newHtml = html.replace('%Name%', object.name);
     newHtml = newHtml.replace('%comment-message%', object.message);
+
+    // insert the new html inside the newdiv
     newDiv.innerHTML = newHtml;
+
+    //
     newSection.appendChild(newDiv);
-    console.log(newSection);
+    
 }
 
 
-//console.log(newDiv);
-
-//let getNewName = document.forms;
-//console.log(getNewName);
-
 function storeComment (event) {
-    console.log(newComment.length + "init len");
+
+    // prevent reloading the page
     event.preventDefault();
 
+    // create an array to store all the comments
+    let newComment = [];
+
+    // store new comment in the object
     let postedContent = {
         postName: event.target.name.value,
         postComment: event.target.message.value
     }
+    
 
     if (postedContent.postName === '' || postedContent.postComment === '') {
-        newComment = newComment
-    } else {
 
-        //store comment
+        //for empty string do nothing
+        newComment = newComment
+
+    } else {
+        //store comment-object in the array
         newComment.unshift(postedContent);
+        
 
         // call the function for displaying comment
-        displayComment();
-
-        
+        displayComment(newComment);
+ 
     }
 
     
 }
 
-function displayComment () {
+function displayComment (array) {
 
-    for (i = 0; i <= (newComment.length - 1); i++)  {
-        let displayContent = {
-            name: newComment[i].postName,
-            message: newComment[i].postComment
-        }
-
-        // call the function for new comment section
-        createCommentDiv(displayContent);
+    //create an objecct that only stores the most recent comment
+    let displayContent = {
+        name: array[0].postName,
+        message: array[0].postComment
     }
-    
 
+    // call the function to create new html section
+    createCommentDiv(displayContent);
+   
 }
 
+// start calling the functions after this event occurs
 document.querySelector('.form').addEventListener('submit', storeComment);
 
-console.log(newComment);
+//console.log(newComment);
