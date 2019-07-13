fb0c456ef09ab8132ed5fa211f21705034d85d00
Noor <noortazc@gmail.com>
2019-07-13 13:24:12 -0400

    displayed comments on page

--- sprint-2/js/script.js
+++ sprint-2/js/script.js
@@ -3,17 +3,22 @@ let newComment = [];
 
 let newSection = document.querySelector('.comments');
 
-let newDiv = document.createElement('div');
-newDiv.classList = 'comments__box';
-let html = newDiv.innerHTML = '<img class="comments__box__image" src="" alt=""><div class="comments__box__content"><div class="comments__box__name">%Name%</div><div class="comments__box__date">%12/11/2019%</div><div class="comments__box__message">%comment-message% </div></div >'
-newSection.appendChild(newDiv);
-let newHtml = html.replace('%Name%', object.name);
-newHtml = newHtml.replace('%comment-message%', object.message)
+function createCommentDiv (object) {
+    let newDiv = document.createElement('div');
+    newDiv.classList = 'comments__box';
+    let html = '<img class="comments__box__image" src="" alt=""><div class="comments__box__content"><div class="comments__box__name">%Name%</div><div class="comments__box__date">%12/11/2019%</div><div class="comments__box__message">%comment-message% </div></div >'
+    let newHtml = html.replace('%Name%', object.name);
+    newHtml = newHtml.replace('%comment-message%', object.message);
+    newDiv.innerHTML = newHtml;
+    newSection.appendChild(newDiv);
+    console.log(newSection);
+}
 
-console.log(newDiv);
 
-let getNewName = document.forms;
-console.log(getNewName);
+//console.log(newDiv);
+
+//let getNewName = document.forms;
+//console.log(getNewName);
 
 function storeComment (event) {
     console.log(newComment.length + "init len");
@@ -27,18 +32,34 @@ function storeComment (event) {
     if (postedContent.postName === '' || postedContent.postComment === '') {
         newComment = newComment
     } else {
+
+        //store comment
         newComment.unshift(postedContent);
+
+        // call the function for displaying comment
+        displayComment();
+
+        
     }
+
+    
 }
 
 function displayComment () {
-    let displayContent = {
-        name: newComment[i].postName,
-        message: newComment[i].postComment
+
+    for (i = 0; i <= (newComment.length - 1); i++)  {
+        let displayContent = {
+            name: newComment[i].postName,
+            message: newComment[i].postComment
+        }
+
+        // call the function for new comment section
+        createCommentDiv(displayContent);
     }
+    
 
 }
 
-document.querySelector('.form').addEventListener('submit', displayComment);
+document.querySelector('.form').addEventListener('submit', storeComment);
 
 console.log(newComment);
